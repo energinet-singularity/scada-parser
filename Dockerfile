6890c8242efc979a845a561e1cfb6e32069f24f6
stigsberg <tsp@energinet.dk>
2021-11-26 10:08:48 +0100

    addded functionality to check for ksql streams/tables is created

--- Dockerfile
+++ Dockerfile
@@ -4,10 +4,10 @@ FROM python:3.10.0-slim-bullseye
 # Install python and pip
 
 # Install Python modules needed by the Python app
-RUN pip3 install kafka-python==2.0.2
+RUN pip3 install kafka-python==2.0.2 requests --no-cache-dir
 
 # Copy files required for the app to run
-COPY dlr_import_amps.py /DLR/
+COPY dlr_import_amps.py ksql-config.json /DLR/
 
 # Declare the port number the container should expose
 
--- dlr_import_amps.py
+++ dlr_import_amps.py
@@ -2,12 +2,14 @@ from time import sleep
 import csv
 import json
 import os
+import requests
 from datetime import datetime
 from json import dumps
 from kafka import KafkaProducer
 
 IP = os.environ['KAFKA_IP']
 topic_name = os.environ['KAFKA_TOPIC']
+ksql_host = os.environ.get('KSQL_HOST', "kafka-cp-ksql-server")
 
 producer = KafkaProducer(bootstrap_servers=[IP],
                          value_serializer=lambda x: 
@@ -23,6 +25,59 @@ show_debug = True
 show_data = True
 # topic_name = 'dlr_scada_amps'
 
+#Function
+def setup_ksql(ksql_host: str, ksql_config: json):
+    #Verifying connection
+    print(f"Validating kSQLdb setup on host '{ksql_host}'..")
+    
+    try:
+        response = requests.get(f"http://{ksql_host}/info")
+    except Exception:
+        print(f"Rest API on 'http://{ksql_host}/info' did not respond as expected. Make sure environment variable 'KSQL_HOST' is correct.")
+        return False
+    
+    if response.status_code == 200:
+        print('Host responded in an orderly fashion..')
+    else:
+        print(f"Rest API on 'http://{ksql_host}/info' did not respond as expected. Make sure environment variable 'KSQL_HOST' is correct.")
+        return False
+
+    #Verifying streams and tables
+    response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"LIST STREAMS; LIST TABLES;", "streamsProperties": {}})
+    if response.status_code == 200:
+        #Create dict with list of known streams and tables
+        ksql_existing_config = {"STREAM": [item['name'] for reply in response.json() if reply['@type'] == 'streams' for item in reply['streams']], "TABLE": [item['name'] for reply in response.json() if reply['@type'] == 'tables' for item in reply['tables']]}
+        for ksql_item in ksql_config['config']:
+            #Check if the item is in the lists returned by kSQL
+            if ksql_item['NAME'] in ksql_existing_config[ksql_item['TYPE']]:
+                #Found - log it, but do nothing
+                print(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' was found.')
+            else:
+                #Not found - try creating it
+                response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"CREATE {ksql_item['TYPE']} {ksql_item['NAME']} {ksql_item['CONFIG']};", "streamsProperties": {}})
+                if response.status_code == 200 and response.json().pop()['commandStatus']['status'] == 'SUCCESS':
+                    print(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' created.')
+                else:
+                    print(f'Problem while trying to create {ksql_item["TYPE"].lower()}  \'{ksql_item["NAME"]}\'.')
+                    return False
+    else:
+        print('Error while gettings streams and tables from kSQL.')    
+        return False
+    
+    print('kSQL setup has been validated.')
+    return True
+
+# Variables (ksql_host should be an environment variables, note the new json-file)
+# ksql_host = "kafka-cp-ksql-server:8088"
+ksql_config = json.loads(open("./DLR/ksql-config.json").read())
+
+# Function call
+if setup_ksql(ksql_host, ksql_config):
+    print("kSQL setup validated/created.")
+else:
+    print("kSQL setup could not be validated/created.")
+
+
 while True:
     sleep(cycle)
     if show_debug: print('Container running')
@@ -34,27 +89,27 @@ while True:
             print('File is missing')
             continue
 
-        #-- Updatere timestamp på filen
+        #-- Update timestamp on file
         csv_from_oag_time = os.stat(file_path).st_mtime
         if show_debug: print(datetime.fromtimestamp(csv_from_oag_time))
         
-        #-- Får nye datasæt
+        #-- Get the new data
         csv_file_time = os.stat(file_path).st_mtime
         csv_file = open(file_path, "r")
         input_data = csv_file.read().splitlines()
         csv_file.close()
 
-        #-- Opsplitning af datasættet
+        #-- Shaping data
         for csv_line in input_data:
             obj_values = [x.strip() for x in csv_line.split(",")]
             json_data.append({"MRID":obj_values[0],"Value":float(obj_values[1]), 
             "Quality":int(obj_values[2]), "Time":datetime.fromtimestamp(csv_file_time).strftime('%Y-%m-%d %H:%M:%S')})
 
-        #-- Print af datasæt til visualisering
+        #-- Debug/data information if needed
         if show_data: print(json.dumps(json_data,indent=4))
         if show_debug: print(type(json_data))
 
-        #-- Sender datasæts til kafka topic
+        #-- Sending data to the given topic name the container was created with
         for i in json_data:
             producer.send(topic_name, value=json.dumps(i))
         if show_debug: print('Import succesfull')
