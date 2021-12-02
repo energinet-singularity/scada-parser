901391b14e22529549255ca5bf5060b7facd1e1d
stigsberg <tsp@energinet.dk>
2021-12-02 15:00:27 +0100

    fixed syntax on comment @jesper

--- dlr_import_amps.py
+++ dlr_import_amps.py
@@ -14,7 +14,7 @@ ksql_host = os.environ.get('KSQL_HOST', "kafka-cp-ksql-server")
 ksql_stream = os.environ.get('KSQL_STREAM')
 ksql_table = os.environ.get('KSQL_TABLE')
 
-#--- Variables and initialization
+# Variables and initialization
 file_path = '/data/test_out.IMP'
 json_data = json.loads('[]')
 csv_from_oag_time = 0
@@ -29,7 +29,7 @@ ksql_config = {
     ]
 }
 
-#-- Function
+# Function
 def setup_ksql(ksql_host: str, ksql_config: json):
     #-- Verifying connection
     print(f"Validating kSQLdb setup on host '{ksql_host}'..")
@@ -46,18 +46,18 @@ def setup_ksql(ksql_host: str, ksql_config: json):
         print(f"Rest API on 'http://{ksql_host}/info' did not respond as expected. Make sure environment variable 'KSQL_HOST' is correct.")
         return False
 
-    #Verifying streams and tables
+    # Verifying streams and tables
     response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"LIST STREAMS; LIST TABLES;", "streamsProperties": {}})
     if response.status_code == 200:
-        #Create dict with list of known streams and tables
+        # Create dict with list of known streams and tables
         ksql_existing_config = {"STREAM": [item['name'] for reply in response.json() if reply['@type'] == 'streams' for item in reply['streams']], "TABLE": [item['name'] for reply in response.json() if reply['@type'] == 'tables' for item in reply['tables']]}
         for ksql_item in ksql_config['config']:
-            #Check if the item is in the lists returned by kSQL
+            # Check if the item is in the lists returned by kSQL
             if ksql_item['NAME'] in ksql_existing_config[ksql_item['TYPE']]:
                 #Found - log it, but do nothing
                 print(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' was found.')
             else:
-                #Not found - try creating it
+                # Not found - try creating it
                 response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"CREATE {ksql_item['TYPE']} {ksql_item['NAME']} {ksql_item['CONFIG']};", "streamsProperties": {}})
                 if response.status_code == 200 and response.json().pop()['commandStatus']['status'] == 'SUCCESS':
                     print(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' created.')
@@ -77,7 +77,7 @@ if setup_ksql(ksql_host, ksql_config):
 else:
     print("kSQL setup could not be validated/created.")
 
-#-- Main function
+# Main function
 try:
     producer = KafkaProducer(bootstrap_servers=[IP],
                             value_serializer=lambda x: 
@@ -89,37 +89,37 @@ except Exception:
 while True:
     sleep(cycle)
     if show_debug: print('Container running')
-    #-- Check for if the given filepath exist
+    # Check for if the given filepath exist
     if not os.path.isfile(file_path):
         print('File is missing')
         continue
-    #-- Start importing data if the file have a new timestamp
+    # Start importing data if the file have a new timestamp
     if os.stat(file_path).st_mtime > csv_from_oag_time:
         if show_debug: print('Import starting')
 
-        #-- Update timestamp on file
+        # Update timestamp on file
         csv_from_oag_time = os.stat(file_path).st_mtime
         if show_debug: print(datetime.fromtimestamp(csv_from_oag_time))
         
-        #-- Get the new data
+        # Get the new data
         csv_file_time = os.stat(file_path).st_mtime
         csv_file = open(file_path, "r")
         input_data = csv_file.read().splitlines()
         csv_file.close()
 
-        #-- Shaping data
+        # Shaping data
         for csv_line in input_data:
             obj_values = [x.strip() for x in csv_line.split(",")]
             json_data.append({"MRID":obj_values[0],"Value":float(obj_values[1]), 
             "Quality":int(obj_values[2]), "Time":datetime.fromtimestamp(csv_file_time).strftime('%Y-%m-%d %H:%M:%S')})
 
-        #-- Debug/data information if needed
+        # Debug/data information if needed
         if show_data: print(json.dumps(json_data,indent=4))
         if show_debug: print(type(json_data))
 
-        #-- Sending data to the given topic name the container was created with
+        # Sending data to the given topic name the container was created with
         for i in json_data:
             producer.send(topic_name, value=json.dumps(i))
         if show_debug: print('Import succesfull')
-        #-- clearing the cache of output data
+        # clearing the cache of output data
         json_data = json.loads('[]')
