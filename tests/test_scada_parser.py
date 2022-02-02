b01940805fe767f8e90c8eb4e86ecdd7fd36498e
stigsberg <tsp@energinet.dk>
2022-02-02 14:43:55 +0100

    added dummy test in tests folder

---
+++ tests/test_scada_parser.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# import pytest
+# import app.kafka_helper_functions
+# import app.lfc_kafka_message_value_names
+# import app.lfc_kafka_topic_names
+# import app.lfc_parameters
+# import app.pbr_demand_respone_simulator
+
+
+# Dummytest which will always succeed - must be replaced by real tests
+def test_dummy():
+    assert True
