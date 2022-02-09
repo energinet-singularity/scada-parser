02cfd95059a052424525b7aab12ed2bbb2131236
Karsten Rasmussen <bolsmand@gmail.com>
2022-02-09 12:49:07 +0100

    Refactor of code

--- app/dlr_import_amps.py
+++ app/dlr_import_amps.py
@@ -5,49 +5,18 @@ import time
 import json
 import logging
 import requests
-from json import dumps
 from datetime import datetime
 from kafka import KafkaProducer
 
 # Initialize log
 log = logging.getLogger(__name__)
 
-try:
-    ip = os.environ.get('KAFKA_IP')
-except KeyError:
-    log.error('Input on KAFKA_IP is not set')
-    sys.exit(1)
 
-try:
-    ksql_host = os.environ.get('KSQL_HOST')
-except KeyError:
-    log.error('Input on KSQL_HOST is not set')
-    sys.exit(1)
-
-try:
-    ksql_table = os.environ.get('KSQL_TABLE')
-except KeyError:
-    log.error('Input on KSQL_TABLE is not set')
-    sys.exit(1)
-
-try:
-    topic_name = os.environ.get('KAFKA_TOPIC')
-except KeyError:
-    log.error('Input on KAFKA_TOPIC is not set')
-    sys.exit(1)
-
-try:
-    ksql_stream = os.environ.get('KSQL_STREAM')
-except KeyError:
-    log.error('Input on KSQL_STREAM is not set')
-    sys.exit(1)
-
-
-# Function
+# Function to setup kSQL output
 def setup_ksql(ksql_host: str, ksql_config: json):
     # Verifying connection
     log.info(f"Validating kSQLdb setup on host '{ksql_host}'..")
-    
+
     try:
         response = requests.get(f"http://{ksql_host}/info")
         if response.status_code != 200:
@@ -58,104 +27,144 @@ def setup_ksql(ksql_host: str, ksql_config: json):
                       " Make sure environment variable 'KSQL_HOST' is correct.")
 
     # Verifying streams and tables
-    response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"LIST STREAMS; LIST TABLES;", "streamsProperties": {}})
+    response = requests.post(f"http://{ksql_host}/ksql", json={"ksql": f"LIST STREAMS; LIST TABLES;", "streamsProperties": {}})
     if response.status_code == 200:
         # Create dict with list of known streams and tables
-        ksql_existing_config = {"STREAM": [item['name'] for reply in response.json() if reply['@type'] == 'streams' for item in reply['streams']], "TABLE": [item['name'] for reply in response.json() if reply['@type'] == 'tables' for item in reply['tables']]}
-        for ksql_item in ksql_config['config']:
+        ksql_existing_config = {"STREAM": [item['name'] for reply in response.json() if reply['@type'] == 'streams'
+                                for item in reply['streams']], "TABLE": [item['name'] for reply in response.json()
+                                if reply['@type'] == 'tables' for item in reply['tables']]}
+        for item in ksql_config['config']:
             # Check if the item is in the lists returned by kSQL
-            if ksql_item['NAME'] in ksql_existing_config[ksql_item['TYPE']]:
+            if item['NAME'] in ksql_existing_config[item['TYPE']]:
                 # Found - log it, but do nothing
-                log.info(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' was found.')
+                log.info(f'{item["TYPE"].capitalize()} \'{item["NAME"]}\' was found.')
             else:
                 # Not found - try creating it
-                response = requests.post(f"http://{ksql_host}/ksql",json={"ksql": f"CREATE {ksql_item['TYPE']} {ksql_item['NAME']} {ksql_item['CONFIG']};", "streamsProperties": {}})
+                response = requests.post(f"http://{ksql_host}/ksql",
+                                         json={"ksql": f"CREATE {item['TYPE']} {item['NAME']} {item['CONFIG']};",
+                                               "streamsProperties": {}})
                 if response.status_code == 200 and response.json().pop()['commandStatus']['status'] == 'SUCCESS':
-                    log.info(f'{ksql_item["TYPE"].capitalize()} \'{ksql_item["NAME"]}\' created.')
+                    log.info(f'{item["TYPE"].capitalize()} \'{item["NAME"]}\' created.')
                 else:
-                    log.error(f'Problem while trying to create {ksql_item["TYPE"].lower()}  \'{ksql_item["NAME"]}\'.')
+                    log.error(f'Problem while trying to create {item["TYPE"].lower()}  \'{item["NAME"]}\'.')
                     return False
     else:
         log.error('Error while gettings streams and tables from kSQL.')
         return False
-    
+
     log.info('kSQL setup validated/created.')
     return True
 
 
+def load_dlr_file(file_path: str) -> dict:
+    log.debug(f"Trying to parse file '{file_path}'.")
 
-# Main loop
+    # Check if the file exist
+    if not os.path.isfile(file_path):
+        log.error('File not found')
+        return
+
+    # Reading and shaping data
+    json_data = json.loads('[]')
+    with open(file_path, 'r', newline='') as f:
+        reader = csv.reader(f, delimiter=',')
+        for row in reader:
+            json_data.append({
+                "MRID": row[0],
+                "Value": float(row[1]),
+                "Quality": int(row[2]),
+                "Time": datetime.fromtimestamp(os.stat(file_path).st_mtime).strftime('%Y-%m-%d %H:%M:%S')
+            })
+
+    return json_data
+
+
+def get_kafka_setup() -> tuple:
+    try:
+        kafka_host = os.environ.get('KAFKA_IP')
+    except KeyError:
+        log.error('KAFKA_IP is required but has not been set')
+        sys.exit(1)
+
+    try:
+        kafka_topic = os.environ.get('KAFKA_TOPIC')
+    except KeyError:
+        log.error('KAFKA_TOPIC is required but has not been set')
+        sys.exit(1)
+
+    try:
+        producer = KafkaProducer(bootstrap_servers=[kafka_host],
+                                 value_serializer=lambda x:
+                                 x.encode('utf-8'))
+    except Exception:
+        log.error("Connection to kafka failed. Check enviroment variable 'KAFKA_IP' and restart the container.")
+        sys.exit(1)
+
+    return producer, kafka_topic
+
+
+# Function that will load the ksql config
+def get_ksql_setup(kafka_topic: str) -> tuple:
+    ksql_host = ""
+
+    try:
+        ksql_host = os.environ.get('KSQL_HOST')
+        ksql_stream = os.environ.get('KSQL_STREAM')
+        ksql_table = os.environ.get('KSQL_TABLE')
+    except KeyError:
+        log.warning('KSQL configuration missing - skipping kSQL setup.')
+
+    ksql_config = {
+        "config": [
+            {"TYPE": "STREAM",
+             "NAME": f"{ksql_stream}",
+             "CONFIG": ("(MRID VARCHAR, VALUE DOUBLE, QUALITY INTEGER, TIME VARCHAR) "
+                        f"WITH (KAFKA_TOPIC='{kafka_topic}', VALUE_FORMAT='JSON')")},
+            {"TYPE": "TABLE",
+             "NAME": f"{ksql_table}",
+             "CONFIG": ("AS SELECT MRID, LATEST_BY_OFFSET(VALUE) AS VALUE_LATEST, LATEST_BY_OFFSET(QUALITY) AS QUALITY_LATEST,"
+                        f" LATEST_BY_OFFSET(TIME) AS TIME_LATEST FROM '{ksql_stream}' GROUP BY MRID EMIT CHANGES")}
+        ]
+    }
+
+    return ksql_host, ksql_config
+
+
+# Initialization for main
 if __name__ == "__main__":
-
     # Setup logging for client output (__main__ should output INFO-level, everything else stays at WARNING)
     logging.basicConfig(format="%(levelname)s:%(asctime)s:%(name)s - %(message)s")
     logging.getLogger(__name__).setLevel(logging.INFO)
 
+    # Load kafka and kSQL configuration
+    producer, kafka_topic = get_kafka_setup()
+    ksql_host, ksql_config = get_ksql_setup(kafka_topic)
+
     # Variables and initialization
     file_path = '/data/DLR_kafka_out.IMP'
-    json_data = json.loads('[]')
     csv_from_oag_time = 0
-    cycle = 5
-
-    ksql_config = {
-        "config":[
-            {"TYPE": "STREAM", "NAME": f"{ksql_stream}", "CONFIG": f"(MRID VARCHAR, VALUE DOUBLE, QUALITY INTEGER, TIME VARCHAR) WITH (KAFKA_TOPIC='{topic_name}', VALUE_FORMAT='JSON')"},
-            {"TYPE": "TABLE", "NAME": f"{ksql_table}", "CONFIG": f"AS SELECT MRID, LATEST_BY_OFFSET(VALUE) AS VALUE_LATEST, LATEST_BY_OFFSET(QUALITY) AS QUALITY_LATEST, LATEST_BY_OFFSET(TIME) AS TIME_LATEST FROM '{ksql_stream}' GROUP BY MRID EMIT CHANGES"}
-        ]
-    }
-
-    # Trying kafka connection on first startup
-    try:
-        producer = KafkaProducer(bootstrap_servers=[ip],
-                                value_serializer=lambda x: 
-                                x.encode('utf-8'))
-    except Exception:
-        log.error("Connection to kafka have failed. Please check enviroment variable 'KAFKA_IP' and if needed recreate the container")
-        sys.exit(1)
+    sleep_time = 5
 
     while True:
-        time.sleep(cycle)
-        start = time.time()
+        # Check kSQL each run - hack because kSQL might loose its config
+        if ksql_host != "":
+            setup_ksql(ksql_host, ksql_config)
 
-        # Starting with checking that the ksql streams and tables needed for the DLR algorithm exist
-        if not setup_ksql(ksql_host, ksql_config):
-            continue
-        
-        log.info('Container running')
+        if os.path.isfile(file_path):
+            if os.stat(file_path).st_mtime > csv_from_oag_time:
+                start = time.time()
+                csv_from_oag_time = os.stat(file_path).st_mtime
+                json_data = load_dlr_file(file_path)
+                log.debug(json.dumps(json_data, indent=4))
 
-        # Check for if the given filepath exist
-        if not os.path.isfile(file_path):
-            log.error('File is missing')
-            continue
+                # Send data to Kafka
+                for i in json_data:
+                    producer.send(kafka_topic, value=json.dumps(i))
 
-        # Start importing data if the file have a new timestamp
-        if os.stat(file_path).st_mtime > csv_from_oag_time:
-            log.info('Import starting')
+                log.debug(f'Runtime was {time.time() - start}')
+            else:
+                log.debug(f"File at '{file_path}' has not changed.")
+        else:
+            log.debug(f"No file found at '{file_path}'.")
 
-            # Update timestamp from file
-            csv_from_oag_time = os.stat(file_path).st_mtime
-            log.info(datetime.fromtimestamp(csv_from_oag_time))
-            
-            # Reading and shaping data
-            with open(file_path, 'r', newline = '') as f:
-                reader = csv.reader(f, delimiter = ',')
-                for row in reader:
-                    json_data.append({
-                        "MRID":row[0],
-                        "Value":float(row[1]), 
-                        "Quality":int(row[2]), 
-                        "Time":datetime.fromtimestamp(csv_from_oag_time).strftime('%Y-%m-%d %H:%M:%S')
-                    })
-
-            # Data information it send to the log
-            log.info(json.dumps(json_data,indent=4))
-
-            # Sending data to the given topic name the container was created with
-            for i in json_data:
-                producer.send(topic_name, value=json.dumps(i))
-            log.info('Import succesfull')
-
-            # Clearing the cache of output data
-            json_data = json.loads('[]')
-            end = time.time()
-            log.info(f'Runtime of the program is {end - start}')
+        time.sleep(sleep_time)
