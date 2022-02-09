8096c119319490f6f06c5b469617f77c31a94489
Karsten Rasmussen <bolsmand@gmail.com>
2022-02-09 12:50:18 +0100

    New test configuration

--- tests/test_scada_parser.py
+++ tests/test_scada_parser.py
@@ -1,6 +1,32 @@
 #!/usr/bin/env python3
-# import pytest
+#import pytest
+import logging
+import app.dlr_import_amps
+import os
+from datetime import datetime
 
-# Dummytest which will always succeed - must be replaced by real tests
-def test_dummy():
-    assert True
+log = logging.getLogger(__name__)
+
+
+# Test of parsing CSV-file
+def test_parsefile(tmpdir, caplog):
+    caplog.set_level(logging.DEBUG)
+    
+    # Create a test-file to send to the parsefile function
+    file_path = os.path.join(tmpdir.mkdir('data').strpath, "input.csv")
+    with open(file_path, "w") as file:
+        for i in range(1,5):
+            file.write(",".join([str(i)*16,str(i*11.1111).rjust(21),"0","\n"]))
+
+    log.debug(f"Created test.file '{file_path}'")
+
+    # Test the output of the function
+    timestamp = datetime.fromtimestamp(os.path.getmtime(file_path)).strftime('%Y-%m-%d %H:%M:%S')
+    test_json = app.dlr_import_amps.load_dlr_file(file_path)
+    for i, item in enumerate(test_json):
+        assert item['MRID'] == str(i+1)*16
+        assert item['Value'] == (i+1)*11.1111
+        assert item['Quality'] == 0
+        assert item['Time'] == timestamp
+
+        log.debug(f"Validated row: {item}")
